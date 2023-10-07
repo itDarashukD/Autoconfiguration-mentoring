c5f47c9cb689f5c0da8cdd20e0ca30047dacd0ba
Dzmitry_Darashuk <itDarashukD@gmail.com>
2023-10-07 16:44:56 +0200

    application and embedded DB has started
    1. add IT for UserServiceImpl

---
+++ src/test/java/com/example/autoconfiguration/AbstractTestSupporter.java
@@ -0,0 +1,47 @@
+package com.example.autoconfiguration;
+
+
+import com.example.autoconfiguration.model.User;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
+
+
+@ActiveProfiles("integration-test")
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes = {
+        AutoconfigurationApplication.class})
+public abstract class AbstractTestSupporter {
+
+
+    protected static final User userWithName444 = new User() {{
+        setId(444L);
+        setName("444");
+        setEmail("444@.444");
+    }};
+
+    private static final String GET_USER_BY_ID = "SELECT * FROM public.\"User\" WHERE id = ?";
+    private static final String IS_EXIST_IN_USER_TABLE = "SELECT count(*) FROM public.\"User\" WHERE id = ?";
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    protected Optional<User> getUserByIdJdbc(long id) {
+        return jdbcTemplate.queryForObject(GET_USER_BY_ID,
+	       new Object[]{id},
+	       ((rs, rowNum) -> Optional.of(new User(rs.getLong("id"),
+		      rs.getString("name"),
+		      rs.getString("email")))));
+    }
+
+    protected Boolean isExistInDB(long id) {
+        int count = jdbcTemplate.queryForObject(IS_EXIST_IN_USER_TABLE,
+	       new Object[]{id},
+	       Integer.class);
+
+        return count > 0;
+    }
+
+}
---
+++ src/test/resources/data.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO public."User"( name, email)
+	VALUES ('name111', 'email@111.com');
+INSERT INTO public."User"( name, email)
+	VALUES ( 'name222', 'email@222.com');
+INSERT INTO public."User"( name, email)
+	VALUES ( 'name333', 'email@333.com');
